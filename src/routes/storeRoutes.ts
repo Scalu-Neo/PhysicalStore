99c26e32f5d00cfb0de8fe34c946eedbfb3421b9
Scalu-Neo <codigomatrix10@gmail.com>
2024-10-31 20:59:05 -0300

    Adicionando mais endpoints

--- src/routes/storeRoutes.ts
+++ src/routes/storeRoutes.ts
@@ -5,8 +5,10 @@ const router = Router();
 
 router.get('/storeNearby', storeController.GetAllStoreNearby)
 router.get('/allStore', storeController.GetAllStore);
+router.get('/getStore/:id', storeController.GetStoreById)
 router.post('/createStore', storeController.PostStore);
 router.delete('/deleteStore/:id', storeController.DeleteStoreById);
+router.put('/updateStore/:id', storeController.UpdateStoreById);
 
 
 export default router;
